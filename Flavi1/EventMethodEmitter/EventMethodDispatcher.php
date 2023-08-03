b5325810d1d2cfed6c3c922d0c85b97f9477628d
flavi1 <flavien.guillon@gmail.com>
2023-08-03 13:44:41 +0200

    Update EventMethodDispatcher.php

--- Flavi1/EventMethodEmitter/EventMethodDispatcher.php
+++ Flavi1/EventMethodEmitter/EventMethodDispatcher.php
@@ -1,46 +1,21 @@
 <?php
 namespace Flavi1\EventMethodEmitter;
-// todo add autoload
 // todo add psr interfaces
 
 class EventMethodDispatcher
-{
-	protected $className = '';
-	protected $methodListeners = [];
+{	
+	protected $listenerProvider = null;
 	
-	public function __construct($className)
+	public function __construct($listenerProvider)
 	{
-		$this->className = $className;
-	}
-	
-	public function addMethodListener($evType, $method, $listener, $order = 0)
-	{
-echo "\nADDED method listener : $evType $method on ".$this->className."\n";
-		if($evType == 'before' or $evType == 'after')
-			$this->methodListeners[$method][$evType][] = [$order, $listener];
-		else {
-			/*TODO THROW ERROR*/
-		}
-	}
-	
-	public function getListenersForEvent($ev)
-	{
-		$result = [];
-		$arr = $this->methodListeners[$ev->method][$ev->type] ?? false;
-		if($arr and count($arr)) {
-			usort($arr, function($a, $b) {
-				return $a[0] <=> $b[0];
-			});
-			foreach($arr as $listener)
-				$result[] = $listener[1];
-		}
-		return $result;
+		$this->listenerProvider = $listenerProvider;
 	}
 	
 	public function dispatch($ev)
 	{
-echo "DISPATCH method event : ".$ev->type." ".$ev->method." on ".$this->className."\n";
-		foreach($this->getListenersForEvent($ev) as $listener)
+		$LP = $this->listenerProvider;
+//echo "DISPATCH method event : ".$ev->type." ".$ev->method." on ".$LP->getClassName()."\n";
+		foreach($LP->getListenersForEvent($ev) as $listener)
 		{
 			$listener($ev);
 		}
