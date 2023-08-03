ae000e1c041cbf2a8a1d3365ecb8196f533e021c
flavi1 <flavien.guillon@gmail.com>
2023-08-03 13:46:41 +0200

    Update EventMethodEmitter_test.php

--- Flavi1/EventMethodEmitter/EventMethodEmitter_test.php
+++ Flavi1/EventMethodEmitter/EventMethodEmitter_test.php
@@ -81,3 +81,59 @@ $A = new A(7,8);
 echo $A->publicM('John', 'Doe');
 $A->protectedM(11,12);
 $A->privateM(13);
+
+
+class Message extends EventMethodEmitter
+{
+	public static function _prepareMessageTo_($firstName, $lastName)
+	{
+		return 'A short message for '.$firstName.'.'."\n";
+	}
+}
+
+Message::addBeforeListener('prepareMessageTo', function($ev) {
+	$firstname = $ev->arguments[0];
+	$ev->arguments[1] = 'Dupont';	// replace lastname before method call.
+	$lastname = $ev->arguments[1];
+	echo 'Dear '.$firstname.' '.$lastname.",\n\n";
+});
+Message::addAfterListener('prepareMessageTo', function($ev) {
+	//var_dump($ev);
+	$lastname = $ev->arguments[1];
+	$ev->response .= "\nSee U later Mr ".$lastname.' !';
+});
+
+echo Message::prepareMessageTo('John', 'Doe');
+
+echo "\n\n\n";
+
+class Friend extends EventMethodEmitter
+{
+	public $firstname = '';
+	public $lastname = '';
+	
+	public function __construct($firstname, $lastname)
+	{
+		$this->firstname = $firstname;
+		$this->lastname = $lastname;
+	}
+	
+	public function _prepareMessage_($msg)
+	{
+		return 'A short message for '.$this->firstname.'.'."\n".$msg."\n";
+	}
+}
+
+Friend::addBeforeListener('prepareMessage', function($ev) {
+	$firstname = $ev->target->firstname;
+	$ev->target->lastname = 'Dupont';	// replace lastname before method call.
+	$lastname = $ev->target->lastname;
+	echo 'Dear '.$firstname.' '.$lastname.",\n\n";
+});
+Friend::addAfterListener('prepareMessage', function($ev) {
+	$lastname = $lastname = $ev->target->lastname;
+	$ev->response .= "\nSee U later Mr ".$lastname.' !';
+});
+
+$john = new Friend('John', 'Doe');
+echo $john->prepareMessage('I just want to say hello to you!');
